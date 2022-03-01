f9d959356bcbcb45be4cc2f873fa15bfcddc94ce
Krzysztof <krzubrycki@gmail.com>
2022-03-01 16:17:54 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartThymeleafFormsApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartThymeleafFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartThymeleafFormsApplication.class, args);
+    }
+
+}
