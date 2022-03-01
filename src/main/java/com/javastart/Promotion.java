33e0a20b8d3fce4e53631953a9219854849cef02
Krzysztof <krzubrycki@gmail.com>
2022-03-01 16:31:21 +0100

    implemented files

--- src/main/java/com/javastart/Promotion.java
+++ src/main/java/com/javastart/Promotion.java
@@ -1,4 +1,43 @@
 package com.javastart;
 
-public class Promotion {
+
+import java.math.BigDecimal;
+
+class Promotion {
+    private String product;
+    private String shop;
+    private BigDecimal price;
+    private BigDecimal discountPrice;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getShop() {
+        return shop;
+    }
+
+    public void setShop(String shop) {
+        this.shop = shop;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(BigDecimal discountPrice) {
+        this.discountPrice = discountPrice;
+    }
 }
